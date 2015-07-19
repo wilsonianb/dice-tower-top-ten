4c3814123427f34a70499bdd4910cad99a54858b
wilsonianb <bwilson@ripple.com>
2015-07-18 23:58:28 -0700

    Configure sequelize postgres db

---
+++ api/services/database.js
@@ -0,0 +1,42 @@
+'use strict';
+
+var fs        = require('fs');
+var path      = require('path');
+var Sequelize = require('sequelize');
+var basename  = path.basename(module.filename);
+
+var dbURL = "";
+
+if (process.env.DATABASE_URL) {
+  dbURL = process.env.DATABASE_URL;
+} else if (process.env.POSTGRES_PORT_5432_TCP_ADDR) {
+  dbURL = 'postgres://postgres:postgres@'+process.env.POSTGRES_PORT_5432_TCP_ADDR+':'+process.env.POSTGRES_PORT_5432_TCP_PORT+'/postgres';
+}
+
+var sequelize = new Sequelize(dbURL, {});
+
+var db        = {};
+
+var MODELS_PATH = path.join(__dirname, '../models')
+
+fs
+  .readdirSync(MODELS_PATH)
+  .filter(function(file) {
+    return (file.indexOf('.') !== 0) && (file !== basename);
+  })
+  .forEach(function(file) {
+    if (file.slice(-3) !== '.js') return;
+    var model = sequelize['import'](path.join(MODELS_PATH, file));
+    db[model.name] = model;
+  });
+
+Object.keys(db).forEach(function(modelName) {
+  if ('associate' in db[modelName]) {
+    db[modelName].associate(db);
+  }
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;
